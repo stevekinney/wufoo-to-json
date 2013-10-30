97a4b97fde22eecf1ff548d5d8ea85e57857ae29
Steve Kinney <hello@stevekinney.net>
2013-10-30 14:41:46 -0400

    Squash bugs

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ var request = require('request');
 var _ = require('lodash');
 var Objectify = require('objectify-arrays');
 
+var format = require('./lib/format');
+var get = require('./lib/get');
+
 module.exports = pullData;
 
-function pullData(id, subdomain, apiKey, callback) {
+function pullData(subdomain, apiKey, id, callback) {
   var objectify = new Objectify();
   var results = [];
 
@@ -15,41 +18,13 @@ function pullData(id, subdomain, apiKey, callback) {
     callback(null, results);
   });
 
-  getFields(id, subdomain, apiKey, function (err, body) {
-    objectify.setHeader(pluckFields(body));
-    getEntries(id, subdomain, apiKey, function (err, body) {
-      pluckEntries(body).forEach(function (el) {
+  get.fields(subdomain, apiKey, id, function (err, body) {
+    objectify.setHeader(format.fields(body));
+    get.entries(subdomain, apiKey, id, function (err, body) {
+      format.entries(body).forEach(function (el) {
         objectify.write(el);
       });
      objectify.end();
     })
   });
 }
-
-function get(type, subdomain, apiKey, id, callback) {
-  var options = {
-    auth: {
-      user: apiKey,
-      pass: 'footastic',
-      sendImmediately: false
-    }
-  };
-  var url = 'https://' + subdomain + '.wufoo.com/api/v3/forms/'
-            + id + '/' + type + '.json';
-  request(url, options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      callback(null, JSON.parse(body));
-    } else {
-      callback(error, body);
-    }
-  });
-}
-
-function getFields(id, subdomain, callback) {
-  get('fields', id, subdomain, callback);
-}
-
-function getEntries(id, subdomain, callback) {
-  get('entries', id, subdomain, callback);
-}
-
---
+++ test.js
@@ -0,0 +1,5 @@
+var pull = require('./index');
+
+pull('councilforeconed', '52NK-XRSU-0QQQ-C00B', 68, function (err, body) {
+  console.log(body)
+})
